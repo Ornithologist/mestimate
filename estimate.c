342f1a49ad6b916afeb2eb92f6c29f579cc69728
Ornithologist <anthing1markmax@gmail.com>
2017-10-29 17:18:30 -0400

    add buffer

--- estimate.c
+++ estimate.c
@@ -7,6 +7,7 @@
 #define NUM_PAGES 20
 #define PAGE_SIZE 4096
 #define TRIGGER_RATIO 2
+#define TRIGGER_BUFFER 20
 
 int triggered = 0;
 int page_size = 0;
@@ -15,6 +16,7 @@ int expected_cpu_ms = 0;
 int main(int argc, char **argv)
 {
     int cur_cpu_ms;
+    int trigger_count = 0;
     unsigned long long count = 0;
 
     if ((page_size = sysconf(_SC_PAGESIZE)) == -1) page_size = PAGE_SIZE;
@@ -22,15 +24,19 @@ int main(int argc, char **argv)
     while (!triggered) {
         ++count;
         cur_cpu_ms = time_memset();
-        if (expected_cpu_ms == 0)
-            expected_cpu_ms = cur_cpu_ms;
+        if (expected_cpu_ms == 0) expected_cpu_ms = cur_cpu_ms;
 
         if ((cur_cpu_ms / expected_cpu_ms) >= TRIGGER_RATIO)
+            ++trigger_count;
+
+        if (trigger_count >= TRIGGER_BUFFER)
             triggered = 1;
     }
 
-    unsigned long long ram_size = count * page_size * NUM_PAGES;
+    unsigned long long ram_size = (count - TRIGGER_BUFFER) * page_size * NUM_PAGES;
     printf("Memory size is approximately %llu bytes\n", ram_size);
+    printf("Expected set time is %d ms, actual set time is %d ms, count is %llu\n",
+           expected_cpu_ms, cur_cpu_ms, count);
 
     return 0;
 }
